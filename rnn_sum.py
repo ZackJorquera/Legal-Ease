0e8bfa8d216ce138916f1b613df5fdc71ba47095
ZackJorquera <jorquerazack@gmail.com>
2021-03-06 18:32:24 -0700

    Re did model
    Changed the model, added a fit function

--- rnn_sum.py
+++ rnn_sum.py
@@ -3,23 +3,193 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
+from datetime import datetime
+
 
 # TODO: move to `__init__` input
-CONFIG_MAX_WORDS = 10000
+CONFIG_MAX_WORDS = 1000
 CONFIG_EMBEDDING_OUTPUT_SIZE = 128
-CONFIG_NUM_CLASSES = 10
 CONFIG_INTERNAL_UNITS = 128
-CONFIG_DECODER_DENSE_OUTPUTS = 10
+CONFIG_DECODER_DENSE_OUTPUTS = 3
 CONFIG_MAX_INPUT_LEN = 10000
 
 
-def get_custom_test_comments():
-    '''
-    Returns a sample test which is easy to manually validates
-    '''
-    print('\nCreating Manual Test...')
+#  help from: https://github.com/chen0040/keras-text-summarization/blob/11df7c7bf30de8ccd8aecef5a551c136c85f0092/keras_text_summarization/library/seq2seq.py#L396
+class RnnSummarizer(object):
 
+    # def __init__(self):
+    #     self.vocab_size = CONFIG_MAX_WORDS
+    #     encoder_input = layers.Input(shape=(None,), name='encoder_input')
+    #     encoder_embedded = layers.Embedding(input_dim=self.vocab_size, output_dim=CONFIG_EMBEDDING_OUTPUT_SIZE,
+    #                                         input_length=CONFIG_MAX_INPUT_LEN)(encoder_input)
+    #     encoder_lstm = layers.LSTM(CONFIG_INTERNAL_UNITS, return_state=True, name="encoder")
+    #     encoder_output, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedded)
+    #     encoder_state = [encoder_state_h, encoder_state_c]
+    #
+    #     decoder_input = layers.Input(shape=(None,), name='decoder_input')
+    #     decoder_embedded = layers.Embedding(input_dim=self.vocab_size, output_dim=CONFIG_EMBEDDING_OUTPUT_SIZE,
+    #                                         input_length=CONFIG_MAX_INPUT_LEN)(decoder_input)
+    #     decoder_lstm = layers.LSTM(units=CONFIG_INTERNAL_UNITS, return_state=True, return_sequences=True,
+    #                                name='decoder')
+    #     decoder_output, _, _ = decoder_lstm(decoder_embedded, initial_state=encoder_state)
+    #
+    #     decoder_dense = layers.Dense(CONFIG_DECODER_DENSE_OUTPUTS)
+    #     dense_output = decoder_dense(decoder_output)
+    #
+    #     self.max_input_seq_length = 1
+    #
+    #     # this is complicated, i will comment (or ask me, i think it understands)
+    #     model = keras.Model([encoder_input, decoder_input], dense_output)
+    #
+    #     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #
+    #     self.model = model
+    #
+    #     # we use the encoder_model and decoder_model to interface with the model
+    #     self.encoder_model = keras.Model(encoder_input, encoder_state)
+    #
+    #     decoder_state_input = [layers.Input(shape=(CONFIG_INTERNAL_UNITS,)),
+    #                            layers.Input(shape=(CONFIG_INTERNAL_UNITS,))]
+    #     decoder_output, decoder_state_h, decoder_state_c = decoder_lstm(decoder_embedded,
+    #                                                                     initial_state=decoder_state_input)
+    #     decoder_state = [decoder_state_h, decoder_state_c]
+    #     dense_output = decoder_dense(decoder_output)
+    #     self.decoder_model = keras.Model([decoder_input] + decoder_state_input, [dense_output] + decoder_state)
+
+    def __init__(self):
+        self.vocab_size = CONFIG_MAX_WORDS
+        # TODO: make Bidirectional: https://www.tensorflow.org/tutorials/text/text_classification_rnn
+        encoder_input = layers.Input(shape=(None,), name='encoder_input')
+        encoder_embedded = layers.Embedding(input_dim=self.vocab_size, output_dim=CONFIG_EMBEDDING_OUTPUT_SIZE,
+                                            input_length=CONFIG_MAX_INPUT_LEN)(encoder_input)
+        encoder_lstm = layers.LSTM(CONFIG_INTERNAL_UNITS, return_state=True, name="encoder")
+        encoder_output, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedded)
+        encoder_state = [encoder_state_h, encoder_state_c]
+
+        decoder_dense = layers.Dense(CONFIG_DECODER_DENSE_OUTPUTS)
+        dense_output = decoder_dense(encoder_output)
+
+        self.max_input_seq_length = 10000
+
+        # this is complicated, i will comment (or ask me, i think it understands)
+        model = keras.Model(encoder_input, dense_output)
+
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        self.model = model
+
+        # we use the encoder_model_get_state and encoder_model_with_state to interface with the model
+        self.encoder_model_get_state = keras.Model(encoder_input, encoder_state)
+
+        encoder_state_input = [layers.Input(shape=(CONFIG_INTERNAL_UNITS,)), layers.Input(shape=(CONFIG_INTERNAL_UNITS,))]
+        encoder_output, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedded, initial_state=encoder_state_input)
+        encoder_state = [encoder_state_h, encoder_state_c]
+        dense_output = decoder_dense(encoder_output)
+        self.encoder_model_with_state = keras.Model([encoder_input] + encoder_state_input, [dense_output] + encoder_state)
+
+    # def summary(self):
+    #     self.model.summary()
+    #     self.encoder_model.summary()
+    #     self.decoder_model.summary()
+
+    def summary(self):
+        self.model.summary()
+        self.encoder_model_get_state.summary()
+        self.encoder_model_with_state.summary()
+
+    def load_weights(self, weight_file_path):
+        if os.path.exists(weight_file_path):
+            self.model.load_weights(weight_file_path)
+
+    def save_weights(self, weight_file_path):
+        self.model.save_weights(weight_file_path)
+
+    def fit(self, x_train, y_train, x_test, y_test, epochs=None, batch_size=None, model_dir_path=None):
+        now = datetime.now()
+        if epochs is None:
+            epochs = 10
+        if model_dir_path is None:
+            model_dir_path = f'./model/checkpoints/{now.strftime("%Y%m%d-%H%M%S")}'
+        if batch_size is None:
+            batch_size = 5
+
+        checkpoint = keras.callbacks.ModelCheckpoint(model_dir_path + '/cp-{epoch}.ckpt', save_weights_only=True)
+        tb_cb = tf.keras.callbacks.TensorBoard(log_dir=f'./logs/{now.strftime("%Y%m%d-%H%M%S")}', update_freq='epoch', profile_batch=2,)
+
+        x_train = [one_hot(x_elem, self.vocab_size) for x_elem in x_train]
+        x_train = pad_sequences(x_train, maxlen=self.max_input_seq_length)
+        x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+
+        y_train = np.array(y_train)
+        y_train = keras.utils.to_categorical(y_train, num_classes=CONFIG_DECODER_DENSE_OUTPUTS)
+
+        if x_test is not None and y_test is not None:
+            x_test = [one_hot(x_elem, self.vocab_size) for x_elem in x_test]
+            x_test = pad_sequences(x_test, maxlen=self.max_input_seq_length)
+            x_test = x_test.reshape((x_test.shape[0], x_test.shape[0], 1))
+
+            y_test = keras.utils.to_categorical(y_test, num_classes=CONFIG_DECODER_DENSE_OUTPUTS)
+        else:
+            x_test, y_test = None, None
+
+        history = self.model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[checkpoint, tb_cb])
+
+        return history
+
+    # def classify(self, sentence_list):
+    #     # TODO: allow for all uppercase to be different work
+    #     encoded_lines = [one_hot(input_text_line, self.vocab_size) for input_text_line in sentence_list]
+    #     encoded_full_doc = [item for sublist in encoded_lines for item in sublist]
+    #     input_seq = np.array(encoded_full_doc).reshape((1,-1,1))
+    #     input_seq_lines = [np.array(encoded_line).reshape((1,-1,1)) for encoded_line in encoded_lines]
+    #     #print(input_seq, input_seq_lines)
+    #
+    #     states_value = self.encoder_model.predict(input_seq)
+    #     output_data = []
+    #     for input_line_seq in input_seq_lines:
+    #         output_tokens, h, c = self.decoder_model.predict([np.asarray(input_line_seq)] + states_value)
+    #
+    #         sample_token_idx = np.argmax(output_tokens[0, -1, :])
+    #         output_data.append(sample_token_idx)
+    #
+    #         states_value = [h, c]
+    #
+    #     return output_data
+
+    def classify(self, sentence_list, normal=True):
+        # TODO: allow for all uppercase to be different work
+        encoded_lines = [one_hot(input_text_line, self.vocab_size) for input_text_line in sentence_list]
+        encoded_full_doc = [item for sublist in encoded_lines for item in sublist]
+        input_seq = np.array(encoded_full_doc).reshape((1,-1,1))
+        input_seq_lines = [np.array(encoded_line).reshape((1,-1,1)) for encoded_line in encoded_lines]
+        #print(input_seq, input_seq_lines)
+
+        states_value = self.encoder_model_get_state.predict(input_seq)
+        output_data = []
+
+        if normal:
+            for input_line_seq in input_seq_lines:
+                output_tokens = self.model.predict([np.asarray(input_line_seq)])
+
+                sample_token_idx = np.argmax(output_tokens[-1, :])
+                output_data.append(sample_token_idx)
+                print(output_tokens)
+        else:
+            for input_line_seq in input_seq_lines:
+                output_tokens, h, c = self.encoder_model_with_state.predict([np.asarray(input_line_seq)] + states_value)
+
+                sample_token_idx = np.argmax(output_tokens[-1, :])
+                output_data.append(sample_token_idx)
+                print(output_tokens)
+
+                states_value = [h, c]
+
+        return output_data
+
+
+if __name__ == '__main__':
     test_comments = [
         "This is a stupid example.",
         "This is another statement, perhaps this will trick the network",
@@ -66,85 +236,12 @@ def get_custom_test_comments():
         "command"
     ]
 
-    return test_comments, test_comments_category
+    test_comments_category = [0 if elem == "statement" else 1 if elem == "question" else 2 for elem in test_comments_category]
 
-
-class RnnSummarizer(object):
-
-    def __init__(self):
-        self.vocab_size = CONFIG_MAX_WORDS
-        encoder_input = layers.Input(shape=(None,), name='encoder_input')
-        encoder_embedded = layers.Embedding(input_dim=self.vocab_size, output_dim=CONFIG_EMBEDDING_OUTPUT_SIZE,
-                                            input_length=CONFIG_MAX_INPUT_LEN)(encoder_input)
-        encoder_lstm = layers.LSTM(CONFIG_INTERNAL_UNITS, return_state=True, name="encoder")
-        encoder_output, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedded)
-        encoder_state = [encoder_state_h, encoder_state_c]
-
-        decoder_input = layers.Input(shape=(None,), name='decoder_input')
-        decoder_embedded = layers.Embedding(input_dim=self.vocab_size, output_dim=CONFIG_EMBEDDING_OUTPUT_SIZE,
-                                            input_length=CONFIG_MAX_INPUT_LEN)(decoder_input)
-        decoder_lstm = layers.LSTM(units=CONFIG_INTERNAL_UNITS, return_state=True, return_sequences=True, name='decoder')
-        decoder_output, _, _ = decoder_lstm(decoder_embedded, initial_state=encoder_state)
-
-        decoder_dense = layers.Dense(CONFIG_DECODER_DENSE_OUTPUTS)
-        dense_output = decoder_dense(decoder_output)
-
-        self.max_input_seq_length = 1
-
-        # this is complicated, i will comment (or ask me, i think it understands)
-        model = keras.Model([encoder_input, decoder_input], dense_output)
-
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-        self.model = model
-
-        # we use the encoder_model and decoder_model to interface with the model
-        self.encoder_model = keras.Model(encoder_input, encoder_state)
-
-        decoder_state_input = [layers.Input(shape=(CONFIG_INTERNAL_UNITS,)), layers.Input(shape=(CONFIG_INTERNAL_UNITS,))]
-        decoder_output, decoder_state_h, decoder_state_c = decoder_lstm(decoder_embedded, initial_state=decoder_state_input)
-        decoder_state = [decoder_state_h, decoder_state_c]
-        dense_output = decoder_dense(decoder_output)
-        self.decoder_model = keras.Model([decoder_input] + decoder_state_input, [dense_output] + decoder_state)
-
-    def summary(self):
-        self.model.summary()
-        self.encoder_model.summary()
-        self.decoder_model.summary()
-
-    def load_weights(self, weight_file_path):
-        if os.path.exists(weight_file_path):
-            self.model.load_weights(weight_file_path)
-
-    def save_weights(self, weight_file_path):
-        self.model.save_weights(weight_file_path)
-
-    def fit(self, x_train, y_train, x_test, y_test):
-        self.model.fit(x_train, y_train, batch_size=128, epochs=5, validation_data=(x_test, y_test))
-
-    def classify(self, input_text):
-        # TODO: allow for all uppercase to be different work
-        # TODO: split bu sentence not line
-        encoded_lines = np.array([one_hot(input_text_line, self.vocab_size) for input_text_line in input_text.splitlines()])
-        encoded_full_doc = [item for sublist in encoded_lines for item in sublist]
-        input_seq = np.array(encoded_full_doc).reshape((1,-1,1))
-        input_seq_lines = [np.array(encoded_line).reshape((1,-1,1)) for encoded_line in encoded_lines]
-        print(input_seq, input_seq_lines)
-
-        states_value = self.encoder_model.predict(input_seq)
-        output_data = []
-        for input_line_seq in input_seq_lines:
-            output_tokens, h, c = self.decoder_model.predict([np.asarray(input_line_seq)] + states_value)
-
-            sample_token_idx = np.argmax(output_tokens[0, -1, :])
-            output_data.append(sample_token_idx)
-
-            states_value = [h, c]
-
-        return output_data
-
-
-if __name__ == '__main__':
     summer = RnnSummarizer()
-    summer.save_weights("model/init.ckpt")
-    print(summer.classify("tes test hello what party.\n hello."))
+    #summer.summary()
+    summer.load_weights("model/final.ckpt")
+    summer.fit(test_comments, test_comments_category, None, None)
+    #summer.save_weights("model/final.ckpt")
+    print(summer.classify(["This is a stupid example.", "Clean the desk.", "What is for lunch"]))
+    # 0, 2, 1
