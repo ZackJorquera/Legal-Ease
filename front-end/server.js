b46867c8b788b69a7d000cd4264f31c74c064d4b
Nadiv Gold <nadivge@gmail.com>
2021-03-06 20:00:37 -0700

    working changes to ui

--- front-end/server.js
+++ front-end/server.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const ejs = require('ejs');
 
 const port = process.env.PORT || 8080
 const app = express()
-app.use(express.static(__dirname + '/src'));
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
 
-app.get('', async (request, response) => {
-  response.send( await readFile(__dirname + '/index.html', 'utf8'));
+app.get('', function(req, res) {
+  res.render('index');
+});
+
+app.get('/contract', function(req, res) {
+
+  let data = "The Parties, as of the date Eclipse formally approves the Project, Party shall cause any related Domain Names (including all sub-domains and related URLs) to redirect directly to the URLs designated by Eclipse with no interstitial content. Within ten (10) days of the Project Effective Date, Party shall transfer to Eclipse Partys entire right, title and interest to the Domain Names. PARTY MAKES NO WARRANTIES, EXPRESS OR IMPLIED, TO ANY PERSON OR ENTITY WITH RESPECT TO THE TRADEMARKS OR ANY RELATED MATERIALS PROVIDED HEREUNDER, ALL OF WHICH ARE PROVIDED AS IS, AND DISCLAIMS ALL IMPLIED WARRANTIES, INCLUDING WITHOUT LIMITATION WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NONINFRINGEMENT. Eclipse may not assign this Agreement or any of its rights or obligations un. this Agreement without the prior written consent of Party. Each party represents and warrants that it has full right, power and authority to enter into this Agreement and perform all of its obligations hereunder. IN WITNESS WHEREOF, the parties hereto have each caused this Agreement to be executed by their authorized rep";
+
+
+  res.render('return', {data} );
 });
 
 app.listen(port)
